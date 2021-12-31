15c480c4b97bbad18e40b75580802bdf9c611fb7
Fitsum <fitbez@gmail.com>
2021-12-30 19:01:11 -0500

    used env variables

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+console.log(process.env.MOGODB_URI);
 
 /* DB connection */
 mongoose
-  .connect(
-    "mongodb+srv://fitbez:123fitbez@cluster0.sjvos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MOGODB_URI)
   .then(() => {
     console.log("DB connection is successfull...");
   })
